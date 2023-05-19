70a629407b1042670f824bca74ed2f2cb315a241
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-05-19 15:16:45 +0300

    fixed: /

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,8 @@ export const App = () => {
 		<Routes>
 			<Route path="/" element={<Layout />}>
 				<Route index element={<HomePage />} />
-				
-					<Route path="tweets" element={<TweetsPage />} />
-
-				<Route path="*" element={<Navigate to="/" replace />} />
+				<Route path="tweets" element={<TweetsPage />} />
+				<Route path="*" element={<Navigate to="/" />} />
 			</Route>
 		</Routes>
 	)
