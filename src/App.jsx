3e215e73925420cbf43f37ed1455302acb96ccc1
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-05-20 09:47:23 +0300

    fixed: home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-//import { lazy } from "react"
-import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/HomePage"
-import TweetsPage from "./pages/TweetsPage"
-import { NotFoundPage } from "./pages/NotFoundPage"
+import { lazy } from "react"
+import { Route, Routes, Navigate } from "react-router-dom"
 import Layout from "./pages/Layout"
 import "./App.css"
 
-// const HomePage = lazy(() => import("./pages/HomePage"))
-// const TweetsPage = lazy(() => import("./pages/TweetsPage"))
+const HomePage = lazy(() => import("./pages/HomePage"))
+const TweetsPage = lazy(() => import("./pages/TweetsPage"))
 
 export const App = () => {
 	return (
@@ -16,7 +13,7 @@ export const App = () => {
 				<Route index element={<HomePage />} />
 				<Route path="tweets" element={<TweetsPage />} />
 			</Route>
-			<Route path="*" element={<NotFoundPage />} />
+			<Route path="*" element={<Navigate to="/" />} />
 		</Routes>
 	)
 }
